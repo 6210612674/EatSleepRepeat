0f661f201aa2e30a9bb3710e7335464199e1afd1
Jirapon Kanpai <6210612690@tu.ac.th>
2021-10-31 15:57:58 +0000

    updateiteration1

--- homepage/models.py
+++ homepage/models.py
@@ -5,18 +5,19 @@ from users.models import User, Store
 # Create your models here.
 class Food (models.Model):
     
-    class Category(models.IntegerChoices):
-        MainCourse = 1
-        Appetizer = 2
-        Dessert = 3
-        Drink = 4
-
+    CATEGORY = [
+        ('Main', 'Main'),
+        ('Appetizer', 'Appetizer'),
+        ('Dessert', 'Dessert'),
+        ('Drink', 'Drink'),
+    ]
+    
     active = models.BooleanField(default=True)
     F_id = models.AutoField(primary_key=True)
     F_name = models.CharField(max_length=150)
     price = models.DecimalField(default=0.00, decimal_places=2, max_digits=10)
     description = models.CharField(max_length=150)
-    category = models.IntegerField(choices = Category.choices)
+    category = models.CharField(max_length=100 ,choices = CATEGORY)
     registered_store = models.ManyToManyField(User, blank=True, related_name="regisstore")
 
     def __str__(self):
