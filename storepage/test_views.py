8ccfe70596161f94f4e2849af9e67f1b3ed28e59
Kotchakorn Surin <6210612674@student.tu.ac.th>
2021-11-07 23:57:38 +0700

    commit Iteration2

--- storepage/test_views.py
+++ storepage/test_views.py
@@ -1,64 +1,67 @@
 from django.test import TestCase, Client
 from django.urls import reverse
-from django.db.models import Max
 from users.models import *
 from homepage.models import *
 csrf_client = Client(enforce_csrf_checks=True)
 
+
 class StorepageViewTestCase(TestCase):
 
     def setUp(self):
-        food = Food.objects.create(F_name="AAA", price=10, description="AAA", category=1)
-
-        userstore = User.objects.create(username="user2340", password="1234", email="user@example.com", is_store=True)
-
-        usercustomer = User.objects.create(username="user2345", password="1234", email="user@example.com", is_customer=True)
-
+        food = Food.objects.create(
+            F_name="AAA", price=10, description="AAA", category=1)
+        userstore = User.objects.create(
+            username="user2340", password="1234", email="user@example.com", is_store=True)
+        usercustomer = User.objects.create(
+            username="user2345", password="1234", email="user@example.com", is_customer=True)
         food.registered_store.add(userstore)
+        food.save()
 
     def test_index_view_status_code(self):
         """check status code"""
 
         c = Client()
         response = c.get(reverse('homepage:index'))
-        self.assertEqual (response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
     def test_addfood_view_page(self):
-        userstore1 = User.objects.create(username="userstore1", password="1234", email="user@example.com", is_store=True)
+        userstore1 = User.objects.create(
+            username="userstore1", password="1234", email="user@example.com", is_store=True)
 
         c = Client()
         c.force_login(userstore1)
         response = c.get(reverse('storepage:addfood'))
-        self.assertEqual (response.status_code, 302)
+        self.assertEqual(response.status_code, 302)
 
     def test_addfood_view_page_notlogin(self):
         c = Client()
         response = c.get(reverse('storepage:addfood'))
-        self.assertEqual (response.status_code, 302)
-
+        self.assertEqual(response.status_code, 302)
 
     def test_statistic_view_page_without_auth(self):
         c = Client()
         response = c.get(reverse('storepage:statistic'))
-        self.assertEqual (response.status_code, 302)
+        self.assertEqual(response.status_code, 302)
 
     def test_statistic_view_page_with_auth(self):
-        userstore = User.objects.create(username="userstore1", password="1234", email="user@example.com", is_store=True)
+        userstore = User.objects.create(
+            username="userstore1", password="1234", email="user@example.com", is_store=True)
         c = Client()
         c.force_login(userstore)
         response = c.get(reverse('storepage:statistic'))
-        self.assertEqual (response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
     def test_storeitem_view_page(self):
-        userstore1 = User.objects.create(username="userstore1", password="1234", email="user@example.com", is_store=True)
-
+        userstore1 = User.objects.create(
+            username="userstore1", password="1234", email="user@example.com", is_store=True)
 
         c = Client()
         response = c.get(reverse('storepage:storeitem', args=(userstore1.id,)))
-        self.assertEqual (response.status_code, 200)
+        self.assertEqual(response.status_code, 200)
 
     def test_addfoodview_view_page(self):
-        userstore = User.objects.create(username="userstore1", password="1234", email="user@example.com", is_store=True)
+        userstore = User.objects.create(
+            username="userstore1", password="1234", email="user@example.com", is_store=True)
 
         c = Client()
         c.force_login(userstore)
@@ -71,10 +74,13 @@ class StorepageViewTestCase(TestCase):
         self.assertEqual(response.status_code, 302)
 
     def test_additemfood_with_auth(self):
-        user = User.objects.create(username="userstore1", password="1234", email="user@example.com", is_store=True)
+        user = User.objects.create(
+            username="userstore1", password="1234", email="user@example.com", is_store=True)
 
         c = Client()
         c.force_login(user)
-        response = c.post(reverse('storepage:addfood'), {'foodname': 'krapra', 'price': 50.0,'type' : 1,'description' : 'aaa'})
+        response = c.post(reverse('storepage:addfood'), {
+                          'foodname': 'krapra', 'price': 50.0, 'type': 1, 'description': 'aaa'})
 
         self.assertEqual(response.status_code, 302)
+
--- users/tests.py
+++ users/tests.py
@@ -2,14 +2,18 @@ from django.test import TestCase, Client
 from django.urls import reverse
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
-from .models import User
+from .models import User, Store
 
 # Create your tests here.
+
 
 class UserViewTestCase(TestCase):
 
     def setUp(self):
-        User.objects.create(username="user1", password=make_password('1234'), email="user1@example.com")
+        User.objects.create(username="user1", password=make_password(
+            '1234'), email="user1@example.com")
+        User.objects.create(username="user2", password=make_password(
+            '1234'), email="user2@example.com", is_store=True)
 
     def test_login_view_with_authentication(self):
         c = Client()
@@ -27,13 +31,15 @@ class UserViewTestCase(TestCase):
     def test_login_success(self):
         c = Client()
         user = User.objects.get(username="user1")
-        response = c.post(reverse('users:login'), {'username': 'user1', 'password': '1234'})
+        response = c.post(reverse('users:login'), {
+                          'username': 'user1', 'password': '1234'})
         self.assertEqual(response.status_code, 302)
 
     def test_login_unsuccess(self):
         c = Client()
         user = User.objects.get(username="user1")
-        response = c.post(reverse('users:login'), {'username': '', 'password': ''})
+        response = c.post(reverse('users:login'), {
+                          'username': '', 'password': ''})
         self.assertEqual(response.status_code, 200)
 
     def test_logout_success(self):
@@ -68,3 +74,12 @@ class UserViewTestCase(TestCase):
         c.force_login(user)
         response = c.get(reverse('users:store'))
         self.assertEqual(response.status_code, 200)
+
+    def test_favourite_successful(self):
+        user1 = User.objects.get(username='user1')
+        user2 = User.objects.get(username='user2')
+        store = Store.objects.get(user=user2)
+        c = Client()
+        c.force_login(user1)
+        response = c.get(reverse('users:favourite', args=(store.user,)))
+        self.assertEqual(store.favourite.count(), 1)
